adcf288d2b7bb1434729dbb1ed3d73f718533165
Sunnyjoe <61793134+Sunnyjoe101@users.noreply.github.com>
2022-02-27 07:52:36 -0500

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,4 @@
-function noOfElements(x, y, z) {}
-
+function noOfElements([2,3,0,1,7], 4, 8) to return 1
+noOfElements([3,3,3,5,18,6,20], 15, 30) to return 2
+noOfElements([4,6,2,6,7,7], 0, 7) to return 6{}
 module.exports = noOfElements
